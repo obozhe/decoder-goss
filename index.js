34cb2c1c47450865edd55c38562acc1a2d4fe237
Narek Avagyan <ava.narek@gmail.com>
2021-12-21 16:00:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,34 +3,36 @@ const PORT = process.env.PORT || 3000;
 const CORS = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'GET,POST,DELETE,OPTIONS',
-  'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, x-test'
+  'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, x-test',
 };
 
-const server = http.createServer(async(request,response)=>{
-  if(request.url === '/result4/'){
+const server = http.createServer(async (request, response) => {
+  if (request.url === '/result4/') {
     let data = '';
-    
-    response.writeHead(200,{
-      'Content-Type':'application/json',
+
+    response.writeHead(200, {
+      'Content-Type': 'application/json',
       ...CORS,
     });
-    
-    await request.on('data', function(chunk){
-      data += chunk;
-      }).on('end', () => {
-    });
-    
-    response.write(JSON.stringify({
-      "message":"artem_wr",
-      "x-result":request.headers["x-test"],
-      "x-body":data
-    }));
+
+    await request
+      .on('data', function (chunk) {
+        data += chunk;
+      })
+      .on('end', () => {});
+
+    response.write(
+      JSON.stringify({
+        message: 'itmo310266',
+        'x-result': request.headers['x-test'],
+        'x-body': data,
+      })
+    );
   }
-  
+
   response.end();
 });
 
-
 server.listen(PORT, () => {
-    console.log(`Server has been started...`);
+  console.log(`Server has been started...`);
 });
